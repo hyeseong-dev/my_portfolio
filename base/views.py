d0de090be2275070dd75a1346eeb714eec434b14
osori <hyeseong43@gmail.com>
2020-08-26 12:17:43 +0900

    1st commit for pofo

---
+++ base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'base/home.html')
+
+def posts(request):
+    return render(request, 'base/posts.html')
+
+def post(request):
+    return render(request, 'base/post.html')
+
+def profile(request):
+    return render(request, 'base/profile.html')
+
